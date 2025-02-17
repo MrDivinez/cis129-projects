52ce59e04e945f1f69acbd5b2187d3710a398c2a
MrDivinez <mmacute@mail.pima.edu>
2025-02-16 17:25:37 -0700

    Love story cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+# Author: Marco Macute
+# My First Python lab  in my CIS129 course
+# This is my simple short love story
+
+print('Once there was a boy, he loved going out to travel but always felt lonely doing so. He would travel across the world and see vast lands to discover, but always wanted to experience this with someone.'),
+print('Until..'),
+print('One day while he was traveling in the Bahamas, he met this girl who loved traveling, but wanted to explore more.'),
+print('So he decided to take her out on a date, and things went very well, they found a lot of things they liked about each other and ended up traveling together'),
+print('Throughout those journeys, they both fell in love and got married in France, where the city of love was met with two couples exploring the world to find each other')
